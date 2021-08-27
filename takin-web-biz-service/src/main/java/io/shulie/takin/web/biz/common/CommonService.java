9973da521c3f3b36d1da399b9416c46fd69c6524
何仲奇 <hezhongqi@shulie.io>
2021-08-27 17:40:13 +0800

    merge code

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/common/CommonService.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/common/CommonService.java
@@ -41,6 +41,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 
 import com.github.pagehelper.Page;
+
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;

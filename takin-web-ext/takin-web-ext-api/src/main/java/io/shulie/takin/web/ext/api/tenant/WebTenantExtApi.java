4674c7d8030b2bf8433486c53bd50de5ae2da3ca
何仲奇 <hezhongqi@shulie.io>
2021-09-27 09:22:18 +0800

    修改

---
+++ takin-web-ext/takin-web-ext-api/src/main/java/io/shulie/takin/web/ext/api/tenant/WebTenantExtApi.java
@@ -0,0 +1,14 @@
+package io.shulie.takin.web.ext.api.tenant;
+
+import org.pf4j.ExtensionPoint;
+
+/**
+ * @author by: hezhongqi
+ * @Package io.shulie.takin.web.ext.api.tenant
+ * @ClassName: WebTenantExtApi
+ * @Description: TODO
+ * @Date: 2021/9/24 17:04
+ */
+public interface WebTenantExtApi extends ExtensionPoint {
+
+}

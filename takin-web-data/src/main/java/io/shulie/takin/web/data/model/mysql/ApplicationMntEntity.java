2b53038c915d20eb562ef8e20bfffbf42fac98c3
何仲奇 <hezhongqi@shulie.io>
2021-09-28 09:30:43 +0800

    修改

--- takin-web-data/src/main/java/io/shulie/takin/web/data/model/mysql/ApplicationMntEntity.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/model/mysql/ApplicationMntEntity.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import io.shulie.takin.web.data.model.mysql.base.TenantBaseEntity;
+import io.shulie.takin.web.data.model.mysql.base.UserBaseEntity;
 import lombok.Data;
 
 /**
@@ -14,7 +14,7 @@ import lombok.Data;
  */
 @Data
 @TableName(value = "t_application_mnt")
-public class ApplicationMntEntity extends TenantBaseEntity {
+public class ApplicationMntEntity extends UserBaseEntity {
 
     /**
      * 主键

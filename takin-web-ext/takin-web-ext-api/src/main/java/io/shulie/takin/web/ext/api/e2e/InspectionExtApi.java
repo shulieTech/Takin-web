36860b99d241273cd2ab8c4dac9ce3446bc7440a
何仲奇 <hezhongqi@shulie.io>
2021-10-11 16:08:44 +0800

    判断e2e插件

---
+++ takin-web-ext/takin-web-ext-api/src/main/java/io/shulie/takin/web/ext/api/e2e/InspectionExtApi.java
@@ -0,0 +1,13 @@
+package io.shulie.takin.web.ext.api.e2e;
+
+import org.pf4j.ExtensionPoint;
+
+/**
+ * @author by: hezhongqi
+ * @Package io.shulie.takin.web.ext.api.e2e
+ * @ClassName: InspectionExtApi
+ * @Description: e2e
+ * @Date: 2021/10/11 16:05
+ */
+public interface InspectionExtApi extends ExtensionPoint {
+}

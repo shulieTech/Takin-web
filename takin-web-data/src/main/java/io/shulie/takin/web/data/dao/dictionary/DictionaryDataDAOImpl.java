895ce9a5a655f0c8dc7a27e8ba3acaadfef00251
caijianying <caijianying@shulie.io>
2021-10-18 18:14:57 +0800

    字典修改

--- takin-web-data/src/main/java/io/shulie/takin/web/data/dao/dictionary/DictionaryDataDAOImpl.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/dao/dictionary/DictionaryDataDAOImpl.java
@@ -36,19 +36,4 @@ public class DictionaryDataDAOImpl implements DictionaryDataDAO{
         }
         return vos.stream().filter(t -> t.getTypeAlias().equals(code)).collect(Collectors.toList());
     }
-
-    @Override
-    public List<TDictionaryVo> getDictByCode(String code, Long tenantId, String envCode) {
-        Map<String, Object> paramMap = Maps.newHashMap();
-        paramMap.put("valueActive", "Y");
-        paramMap.put("typeAlias", code);
-        // 传入租户 环境
-        paramMap.put("tenantId", tenantId);
-        paramMap.put("envCode", envCode);
-        List<TDictionaryVo> vos = tDictionaryDataMapper.queryDictionaryList(paramMap);
-        if(CollectionUtils.isEmpty(vos)) {
-            return Lists.newArrayList();
-        }
-        return vos.stream().filter(t -> t.getTypeAlias().equals(code)).collect(Collectors.toList());
-    }
 }

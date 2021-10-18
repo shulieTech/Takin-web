cbec4c97ccf31fab5df45ca7cc62dacca4439a64
caijianying <caijianying@shulie.io>
2021-10-18 18:13:26 +0800

    字典修改

--- takin-web-data/src/main/java/io/shulie/takin/web/data/dao/dictionary/DictionaryDataDAOImpl.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/dao/dictionary/DictionaryDataDAOImpl.java
@@ -10,6 +10,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.pamirs.takin.entity.dao.dict.TDictionaryDataMapper;
 import com.pamirs.takin.entity.domain.vo.TDictionaryVo;
+import io.shulie.takin.web.ext.util.WebPluginUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
@@ -27,6 +28,8 @@ public class DictionaryDataDAOImpl implements DictionaryDataDAO{
         Map<String, Object> paramMap = Maps.newHashMap();
         paramMap.put("valueActive", "Y");
         paramMap.put("typeAlias", code);
+        paramMap.put("tenantId", WebPluginUtils.traceTenantId());
+        paramMap.put("envCode", WebPluginUtils.traceEnvCode());
         List<TDictionaryVo> vos = tDictionaryDataMapper.queryDictionaryList(paramMap);
         if(CollectionUtils.isEmpty(vos)) {
             return Lists.newArrayList();

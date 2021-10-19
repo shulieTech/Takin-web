74262a7cd3154d01b2863530cd8c860725717776
何仲奇 <hezhongqi@shulie.io>
2021-10-19 11:32:27 +0800

    mybatis 补充拦截

---
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/conf/mybatis/TakinTenantLineHandler.java
@@ -0,0 +1,44 @@
+package io.shulie.takin.web.app.conf.mybatis;
+
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
+import net.sf.jsqlparser.expression.Expression;
+
+/**
+ * @author by: hezhongqi
+ * @Package io.shulie.takin.web.app.conf.mybatis
+ * @ClassName: MyTenantLineHandler
+ * @Description: 自定义租户插件
+ * @Date: 2021/10/18 18:06
+ */
+public interface TakinTenantLineHandler extends TenantLineHandler {
+
+    /**
+     * 环境 code
+     * @return
+     */
+    Expression getEnvCode();
+
+    /**
+     * 默认 字段名
+     * @return
+     */
+    default String getEnvCodeColumn() {
+        return TenantField.FIELD_ENV_CODE.getColumnName();
+    }
+
+
+    /**
+     * 用户id
+     * @return
+     */
+    Expression getUserId();
+
+    /**
+     * 用户字段
+     * @return
+     */
+    default String getUserIdColumn() {
+        return TenantField.FIELD_USER_ID.getColumnName();
+    }
+
+}

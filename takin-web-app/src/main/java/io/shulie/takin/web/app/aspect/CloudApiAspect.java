ebe6efc0db91701496a170f478344a5b024f80cb
张天赐 <472546172@qq.com>
2021-10-21 22:48:33 +0800

    利用切片在调用Cloud时补充数据

--- takin-web-app/src/main/java/io/shulie/takin/web/app/aspect/CloudApiAspect.java
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/aspect/CloudApiAspect.java
@@ -33,4 +33,20 @@ public class CloudApiAspect {
             }
         }
     }
+
+    @Pointcut("execution(public * io.shulie.takin.cloud.sdk.impl..*.*(..))")
+    public void sdk() {
+
+    }
+
+    @Before("sdk()")
+    public void beforeSdk(JoinPoint joinPoint) {
+        Object[] params = joinPoint.getArgs();
+        if (params != null && params.length == 1) {
+            if (params[0] instanceof ContextExt) {
+                ContextExt inParam = (ContextExt)params[0];
+                WebPluginUtils.fillCloudUserData(inParam);
+            }
+        }
+    }
 }

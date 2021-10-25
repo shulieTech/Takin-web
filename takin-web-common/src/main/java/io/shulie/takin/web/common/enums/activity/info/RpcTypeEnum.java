8a11e3ed3bd0f33a2cae8221f4fb1d379f7bab2f
caijianying <caijianying@shulie.io>
2021-10-25 13:52:23 +0800

    合并

---
+++ takin-web-common/src/main/java/io/shulie/takin/web/common/enums/activity/info/RpcTypeEnum.java
@@ -0,0 +1,23 @@
+package io.shulie.takin.web.common.enums.activity.info;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public enum RpcTypeEnum {
+
+    APP("0"),
+    HTTP("0"),
+    HTTP2("1"),
+    DUBBO("1"),
+    MQ("3"),
+    DB("4"),
+    CACHE("5"),
+    UNKNOWN("6"),
+    ;
+
+    private final String value;
+
+}

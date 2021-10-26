69e5e18d8f3689459f5d02c8e8ec7bbdb1639eb7
何仲奇 <hezhongqi@shulie.io>
2021-10-26 17:50:36 +0800

    修改

--- takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/PressureTestEngineConfigMapper.java
+++
@@ -1,6 +0,0 @@
-package io.shulie.takin.web.data.mapper.mysql;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface PressureTestEngineConfigMapper extends BaseMapper<PressureTestEngineConfigEntity> {
-}

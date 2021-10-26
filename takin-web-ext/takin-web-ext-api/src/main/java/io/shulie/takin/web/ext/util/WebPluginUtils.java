48089d0e2139b80272dbb3e6b7534a31c16a3893
caijianying <caijianying@shulie.io>
2021-10-26 16:26:36 +0800

    租户隔离 租户ID默认为1

--- takin-web-ext/takin-web-ext-api/src/main/java/io/shulie/takin/web/ext/util/WebPluginUtils.java
+++ takin-web-ext/takin-web-ext-api/src/main/java/io/shulie/takin/web/ext/util/WebPluginUtils.java
@@ -37,7 +37,7 @@ public class WebPluginUtils {
      * 默认 userAppkey 解决zk PATH 问题
      */
     public static String DEFAULT_TENANT_APP_KEY = "default";
-    public static Long DEFAULT_TENANT_ID = 0L;
+    public static Long DEFAULT_TENANT_ID = 1L;
     public static String DEFAULT_ENV_CODE = "test";
     public static String DEFAULT_TENANT_CODE = "default";
 

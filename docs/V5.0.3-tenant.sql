ffd3ac4727c49da103a10b484ed3501b5b95a7b5
兮曦 <18679155605@163.com>
2021-10-29 11:48:11 +0800

    e2e数据订正sql

--- docs/V5.0.3-tenant.sql
+++ docs/V5.0.3-tenant.sql
@@ -843,7 +843,10 @@ update e_patrol_exception set env_code='test',tenant_id=1;
 
 -- e_patrol_exception_config 瓶颈配置表
 alter table e_patrol_exception_config modify column `customer_id` bigint(20) DEFAULT NULL COMMENT '租户id(已废弃)';
-update e_patrol_exception_config set env_code='test',tenant_id=customer_id;
+update e_patrol_exception_config set tenant_id=customer_id;
+update e_patrol_exception_config set env_code='test' where tenant_id is not null;
+INSERT INTO `trodb`.`e_patrol_exception_config`(`order_number`, `type_value`, `level_value`, `threshold_value`, `contrast_factor`, `remarks`) VALUES (1, 4, 1, 30, 1, '一般慢SQL');
+INSERT INTO `trodb`.`e_patrol_exception_config`(`order_number`, `type_value`, `level_value`, `threshold_value`, `contrast_factor`, `remarks`) VALUES (0, 4, 2, 60, 1, '严重慢SQL');
 ALTER TABLE `e_patrol_exception_config` ADD UNIQUE INDEX `idx_config` ( `env_code`, `tenant_id`, `type_value`,`level_value` ) USING BTREE
 
 -- e_patrol_exception_notice_config 瓶颈通知配置表

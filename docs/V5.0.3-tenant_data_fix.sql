cff6e7955cd0247c79105ea7fbffcf67c65a5249
何仲奇 <hezhongqi@shulie.io>
2021-11-10 17:52:11 +0800

    数据订正

--- docs/V5.0.3-tenant_data_fix.sql
+++ docs/V5.0.3-tenant_data_fix.sql
@@ -271,7 +271,7 @@ INSERT INTO `t_tenant_info`
 VALUES (1, 'ed45ef6b-bf94-48fa-b0c0-15e0285365d2', 'default', 'default', 'default', 1),
     (2, 'ed45ef6b-bf94-48fa-b0c0-15e0285365d3', 'test', '测试', 'test', 1),
     (3, '4d2a89a0-204d-4eab-a031-d9d55eaafc3c', 'T3Trip', 'T3出行', 'T3Trip', 1),
-    (5, '5b06060a-17cb-4588-bb71-edd7f65035aa', 'lbx-pressure', 'lbx-pressure', 'lbx-pressure', 1),
+    (5, '5b06060a-17cb-4588-bb71-edd7f65035aa', 'lbx-pressure', '老百姓', 'lbx-pressure', 1),
     (7, '292e98d4-9efe-4e24-a0c1-e906fa568009', 'sto', '申通快递', 'sto', 1),
     (10, '4beee59d-e8f5-497e-a487-dbbb75194dde', 'liandongyun', '联动云', 'liandongyun', 1),
     (24, '89e425fc-feaa-4983-91c2-8a6c877afb03', 'akc', '爱库存', 'akc', 1);
@@ -296,7 +296,242 @@ VALUES
     (24, 'prod', '生产环境', '当前环境为生产环境，请谨慎操作');
 
 -- 更正t_tro_user
+update  t_tro_user set tenant_id = customer_id
+-- 默认租户
+INSERT INTO t_tro_user
+( `name`, `nick`, `salt`, `password`, `status`, `user_type`, `model`, `role`, `tenant_id` )
+VALUES ( 'admin@default', 'default', '$2a$10$MqkhiWRgANNXPlhM6uJdBe', '$2a$10$MqkhiWRgANNXPlhM6uJdBe0pT.SUmd9JdK9eIFp9CbcZSlOiAWL7S', 0, 2, 0, 0,1);
+-- 测试租户
+INSERT INTO `trodb`.`t_tro_user`
+( `name`, `nick`, `salt`, `password`, `status`, `user_type`, `model`, `role`, `tenant_id` )
+VALUES ( 'admin@test', '测试', '$2a$10$MqkhiWRgANNXPlhM6uJdBe', '$2a$10$MqkhiWRgANNXPlhM6uJdBe0pT.SUmd9JdK9eIFp9CbcZSlOiAWL7S', 0, 2, 0, 0,2);
+-- T3出行出现
+update  t_tro_user  set tenant_id = 3 WHERE name like 'T3Trip%'
+update  t_tro_user  set user_type = 2  WHERE id = 3
+-- 老百姓
+update  t_tro_user  set tenant_id = 5 WHERE name like 'lbx%'
+update  t_tro_user  set user_type = 2  WHERE id = 5
+-- 申通账号
+update  t_tro_user  set tenant_id = 7 WHERE name like 'sto%'
+update  t_tro_user  set user_type = 2  WHERE id = 7
+-- 联动云
+update  t_tro_user  set tenant_id = 1O WHERE name like 'liandongyun%'
+update  t_tro_user  set user_type = 2  WHERE id = 10
+-- 爱库存
+update  t_tro_user  set user_type = 2  WHERE id = 24
+
+-- 修正部门表
+
+-- 部门不区分环境
+update t_tro_user_dept_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id
+update t_tro_dept t1 left join (SELECT DISTINCT  dept_id,tenant_id FROM t_tro_user_dept_relation ) t2 on t1.id = t2.dept_id set t1.tenant_id =  t2.tenant_id
+
+-- 角色 区分 环境  t_tro_authority
+select GROUP_CONCAT(id)  from t_tenant_info
+update t_tro_role_user_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code = 'prod'
+WHERE t1.user_id in(2,3,5,7,10,24)
+
+-- 默认租户
+update t_tro_role_user_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code = 'test'
+WHERE t1.tenant_id = 1
+
+-- T3出行
+update t_tro_role_user_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code = 'test'
+WHERE t1.user_id = 4
+
+-- 老板姓
+update t_tro_role_user_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code = 'prod'
+WHERE t1.user_id in(12,13)
+
+-- 联动云
+update t_tro_role_user_relation t1 left join t_tro_user t2 on t1.user_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code = 'test'
+WHERE t1.user_id = 9
+
+
+update t_tro_role t1 left join (SELECT DISTINCT  role_id,tenant_id FROM t_tro_role_user_relation ) t2 on t1.id = t2.role_id set t1.tenant_id =  t2.tenant_id
+and t1.env_code =  t2.env_code
+
+update t_tro_authority t1 left join t_tro_role  t2 on t1.role_id = t2.id set t1.tenant_id =  t2.tenant_id ,t1.env_code =  t2.env_code
+
+
+
+--
+
+----------------------------更新修正报告相关
+-- 更新 t_report_application_summary |t_report_machine |t_report_summary --
+-- 默认租户 --
+-- 找出默认租户用户id
+SELECT GROUP_CONCAT(id) from t_tro_user WHERE tenant_id = 1
+-- 找出默认租户的报告id trodb_cloud找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id in()
+-- 更新报告 t_report_application_summary
+update t_report_application_summary set tenant_id = 1 and env_code = 'test' WHERE report_id in ()
+update t_report_machine set tenant_id = 1 and env_code = 'test' WHERE report_id in ()
+update t_report_summary set tenant_id = 1 and env_code = 'test' WHERE report_id in ()
+update t_trace_manage set tenant_id = 1 and env_code = 'test' WHERE report_id in ()
+
+-- T3出行
+-- 测试环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id = 4
+update t_report_application_summary set tenant_id = 3 and env_code = 'test' WHERE report_id in ()
+update t_report_machine set tenant_id = 3 and env_code = 'test' WHERE report_id in ()
+update t_report_summary set tenant_id = 3 and env_code = 'test' WHERE report_id in ()
+update t_trace_manage set tenant_id = 3 and env_code = 'test' WHERE report_id in ()
+
+-- 生产环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id = 3
+update t_report_application_summary set tenant_id = 3 and env_code = 'prod' WHERE report_id in ()
+update t_report_machine set tenant_id = 3 and env_code = 'prod' WHERE report_id in ()
+update t_report_summary set tenant_id = 3 and env_code = 'prod' WHERE report_id in ()
+update t_trace_manage set tenant_id = 3 and env_code = 'prod' WHERE report_id in ()
+
+
+
+
+-- 老百姓
+-- 生产环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id in(5,12,13)
+update t_report_application_summary set tenant_id = 5 and env_code = 'prod' WHERE report_id in ()
+update t_report_machine set tenant_id = 5 and env_code = 'prod' WHERE report_id in ()
+update t_report_summary set tenant_id = 5 and env_code = 'prod' WHERE report_id in ()
+update t_trace_manage set tenant_id = 5 and env_code = 'prod' WHERE report_id in ()
+
+
+
+-- 申通快递
+-- 测试环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_tro_user WHERE name like 'sto%' and id <> 7
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id in()
+update t_report_application_summary set tenant_id = 7 and env_code = 'test' WHERE report_id in ()
+update t_report_machine set tenant_id = 7 and env_code = 'test' WHERE report_id in ()
+update t_report_summary set tenant_id = 7 and env_code = 'test' WHERE report_id in ()
+update t_trace_manage set tenant_id = 7 and env_code = 'test' WHERE report_id in ()
+
+-- 生产环境 cloud查找
+update t_report_application_summary set tenant_id = 7 and env_code = 'prod' WHERE report_id in ()
+update t_report_machine set tenant_id = 7 and env_code = 'prod' WHERE report_id in ()
+update t_report_summary set tenant_id = 7 and env_code = 'prod' WHERE report_id in ()
+update t_trace_manage set tenant_id = 7 and env_code = 'prod' WHERE report_id in ()
+
+-- 联动云
+-- 测试环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id = 9
+update t_report_application_summary set tenant_id = 10 and env_code = 'test' WHERE report_id in ()
+update t_report_machine set tenant_id = 10 and env_code = 'test' WHERE report_id in ()
+update t_report_summary set tenant_id = 10 and env_code = 'test' WHERE report_id in ()
+update t_trace_manage set tenant_id = 10 and env_code = 'test' WHERE report_id in ()
+
+
+-- 生产环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id = 10
+update t_report_application_summary set tenant_id = 10 and env_code = 'prod' WHERE report_id in ()
+update t_report_machine set tenant_id = 10 and env_code = 'prod' WHERE report_id in ()
+update t_report_summary set tenant_id = 10 and env_code = 'prod' WHERE report_id in ()
+update t_trace_manage set tenant_id = 10 and env_code = 'prod' WHERE report_id in ()
+
+-- 爱库存
+-- 生产环境 cloud查找
+SELECT GROUP_CONCAT(id) from t_report WHERE user_id = 24
+update t_report_application_summary set tenant_id = 24 and env_code = 'prod' WHERE report_id in ()
+update t_report_machine set tenant_id = 24 and env_code = 'prod' WHERE report_id in ()
+update t_report_summary set tenant_id = 24 and env_code = 'prod' WHERE report_id in ()
+update t_trace_manage set tenant_id = 24 and env_code = 'prod' WHERE report_id in ()
+
+
+UPDATE t_trace_manage_deploy t1
+    LEFT JOIN t_trace_manage t2 ON t1.trace_manage_id = t2.id
+    SET t1.env_code = t2.env_code ,t1.tenant_id = t2.TENANT_ID;
+
+---------------
+-- 场景表 t_scene | t_shadow_job_config | t_shadow_mq_consumer
+-- 查找租户
+select GROUP_CONCAT(id)  from t_tenant_info
+update t_scene set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_shadow_job_config set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_shadow_mq_consumer set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_trace_node_info set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_tro_dbresource set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_white_list set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_whitelist_effective_app set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_application_mnt set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_application_ds_cache_manage set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_application_ds_db_manage set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+
+-- 默认
+update t_scene set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_shadow_job_config set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_shadow_mq_consumer set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_trace_node_info set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_tro_dbresource set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_white_list set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_whitelist_effective_app set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_application_mnt set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_application_ds_cache_manage set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_application_ds_db_manage set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+-- T3出行
+update t_scene set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_shadow_job_config set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_shadow_mq_consumer set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_trace_node_info set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_tro_dbresource set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_white_list set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_whitelist_effective_app set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_application_mnt set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_application_ds_cache_manage set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_application_ds_db_manage set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+-- 老板姓
+update t_scene set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_shadow_job_config set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_shadow_mq_consumer set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_trace_node_info set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_tro_dbresource set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_white_list set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_whitelist_effective_app set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_application_mnt set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_application_ds_cache_manage set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_application_ds_db_manage set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+
+-- 联动云
+update t_scene set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_shadow_job_config set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_shadow_mq_consumer set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_trace_node_info set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_tro_dbresource set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_white_list set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_whitelist_effective_app set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_application_mnt set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_application_ds_cache_manage set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_application_ds_db_manage set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+-- 更新 t_scene_link_relate
+UPDATE t_scene_link_relate t1
+    LEFT JOIN t_scene t2 ON t1.SCENE_ID = t2.id
+    SET t1.env_code = t2.env_code ,t1.tenant_id = t2.TENANT_ID;
+
+------脚本相关------
+-- t_script_debug
+select GROUP_CONCAT(id)  from t_tenant_info
+update t_script_debug set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+update t_script_manage set tenant_id = customer_id and env_code = 'prod' where customer_id in(2,3,5,7,10,24)
+-- 默认
+update t_script_debug set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+update t_script_manage set tenant_id = customer_id and env_code = 'test' where customer_id = 1
+-- T3出行
+update t_script_debug set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+update t_script_manage set tenant_id = 3 and env_code = 'test' where  customer_id = 4
+-- 老板姓
+update t_script_debug set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+update t_script_manage set tenant_id = 5 and env_code = 'prod' where  customer_id in(12,13)
+-- 联动云
+update t_script_debug set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+update t_script_manage set tenant_id = 10 and env_code = 'test' where  customer_id = 9
+
+UPDATE t_script_manage_deploy t1
+    LEFT JOIN t_script_manage t2 ON t1.script_id = t2.id
+    SET t1.env_code = t2.env_code ,t1.tenant_id = t2.TENANT_ID;
+
+-- t_shadow_job_config
 
 
 
 ---无涯
+

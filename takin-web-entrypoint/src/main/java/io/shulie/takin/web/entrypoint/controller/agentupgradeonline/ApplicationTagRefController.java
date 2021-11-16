65d29855fdfdcfe7cc96e7dd9b9eb5ac7f3b5cb6
iaoyz <knightzhangdev@163.com>
2021-11-16 22:59:03 +0800

    探针列表页面添加搜索条件下拉数据

--- takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/agentupgradeonline/ApplicationTagRefController.java
+++ takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/agentupgradeonline/ApplicationTagRefController.java
@@ -1,14 +1,16 @@
 package io.shulie.takin.web.entrypoint.controller.agentupgradeonline;
 
+import io.shulie.takin.common.beans.component.SelectVO;
+import io.shulie.takin.web.biz.service.agentupgradeonline.ApplicationTagRefService;
 import io.shulie.takin.web.common.constant.APIUrls;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 应用标签表(ApplicationTagRef)controller
@@ -17,18 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021-11-09 20:09:43
  */
 @RestController
-@RequestMapping(APIUrls.TAKIN_API_URL + "applicationTagRef")
+@RequestMapping(APIUrls.TAKIN_API_URL + "application/tag")
 @Api(tags = "")
 public class ApplicationTagRefController {
 
-    @ApiOperation("")
-    @ApiImplicitParams({
-        @ApiImplicitParam(name = "id", value = "id", required = true,
-            dataType = "long", paramType = "query")
-    })
-    @GetMapping
-    public void index(@RequestParam Long id) {
+    @Autowired
+    private ApplicationTagRefService tagRefService;
 
+    @ApiOperation("获取标签列表数据")
+    @GetMapping("/list")
+    public List<SelectVO> list() {
+       return tagRefService.getListByTenant();
     }
 
 }

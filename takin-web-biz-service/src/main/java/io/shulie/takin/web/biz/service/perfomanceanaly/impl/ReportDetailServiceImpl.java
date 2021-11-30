fd6f8823d28798d2b891e939feeebc3db2176439
何仲奇 <hezhongqi@shulie.io>
2021-11-30 20:17:12 +0800

    优化

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/perfomanceanaly/impl/ReportDetailServiceImpl.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/perfomanceanaly/impl/ReportDetailServiceImpl.java
@@ -16,15 +16,13 @@ import com.pamirs.takin.entity.domain.entity.TApplicationMnt;
 import io.shulie.takin.web.biz.pojo.response.perfomanceanaly.ReportTimeResponse;
 import io.shulie.takin.web.biz.service.perfomanceanaly.ReportDetailService;
 import io.shulie.takin.web.biz.service.report.impl.ReportApplicationService;
-import io.shulie.takin.web.common.exception.ExceptionCode;
-import io.shulie.takin.web.common.exception.TakinWebException;
-import io.shulie.takin.web.common.exception.TakinWebExceptionEnum;
 import io.shulie.takin.web.data.dao.application.AppAgentConfigReportDAO;
 import io.shulie.takin.web.data.param.application.ConfigReportInputParam;
 import io.shulie.takin.web.data.param.application.CreateAppAgentConfigReportParam;
 import io.shulie.takin.web.data.param.application.UpdateAppAgentConfigReportParam;
 import io.shulie.takin.web.data.result.application.AppAgentConfigReportDetailResult;
 import io.shulie.takin.web.ext.util.WebPluginUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,6 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2020/11/9 下午4:40
  */
 @Service
+@Slf4j
 public class ReportDetailServiceImpl implements ReportDetailService {
 
     @Autowired
@@ -66,12 +65,13 @@ public class ReportDetailServiceImpl implements ReportDetailService {
     @Override
     public void uploadConfigInfo(ConfigReportInputParam inputParam) {
         if (inputParam == null) {
-            throw new TakinWebException(TakinWebExceptionEnum.AGENT_UPLOAD_CONFIG_MSG_ERROR, "上传参数不能为空！");
+            log.error("uploadConfigInfo 上传参数不能为空");
+            return;
         }
         TApplicationMnt info = tApplicationMntDao.queryApplicationinfoByName(inputParam.getApplicationName());
         if (Objects.isNull(info)) {
-            throw new TakinWebException(ExceptionCode.REMOTE_CALL_CONFIG_CHECK_ERROR,
-                "应用【" + inputParam.getApplicationName() + "】不存在");
+            log.error("uploadConfigInfo 应用【{}】不存在",inputParam.getApplicationName());
+            return ;
         }
 
         List<CreateAppAgentConfigReportParam> saveList = new ArrayList<>();

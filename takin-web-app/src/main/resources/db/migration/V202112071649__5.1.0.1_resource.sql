084b4984d384e61fa5429a76e3d0d90f4d857ab2
何仲奇 <hezhongqi@shulie.io>
2021-12-08 16:14:52 +0800

    压测场景里面sa的值如果大于100按照100显示 &消费者页面搜索不消费内容，把原本已经消费的识别成不消费

--- takin-web-app/src/main/resources/db/migration/V202112071649__5.1.0.1_resource.sql
+++ takin-web-app/src/main/resources/db/migration/V202112071649__5.1.0.1_resource.sql
@@ -10,3 +10,28 @@ END IF;
 END $$
 DELIMITER ;
 CALL add_column;
+
+
+alter table t_tro_user_dept_relation  modify column user_id bigint;
+
+alter table t_tro_user_dept_relation  modify column dept_id bigint;
+
+drop index idx_dept_id on t_tro_user_dept_relation;
+drop index idx_user_id on t_tro_user_dept_relation ;
+create index idx_dept_id
+    on t_tro_user_dept_relation (dept_id);
+create index idx_user_id
+    on t_tro_user_dept_relation (user_id);
+
+alter table t_tro_role_user_relation  modify column user_id bigint;
+
+alter table t_tro_role_user_relation  modify column role_id bigint;
+
+drop index idx_role_id on t_tro_role_user_relation;
+drop index idx_user_id on t_tro_role_user_relation ;
+
+create index idx_role_id
+    on t_tro_role_user_relation (role_id);
+
+create index idx_user_id
+    on t_tro_role_user_relation (user_id);

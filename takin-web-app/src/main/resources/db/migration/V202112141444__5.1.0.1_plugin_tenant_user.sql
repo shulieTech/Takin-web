1dc119d2f9def62ee723606916ccae2c2a2841c4
caijianying <caijianying@shulie.io>
2021-12-20 10:30:54 +0800

    fix

--- takin-web-app/src/main/resources/db/migration/V202112141444__5.1.0.1_plugin_tenant_user.sql
+++ takin-web-app/src/main/resources/db/migration/V202112141444__5.1.0.1_plugin_tenant_user.sql
@@ -21,8 +21,8 @@ CALL add_column;
 ALTER TABLE `t_tenant_info`
     MODIFY COLUMN `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '状态 0: 停用 1:正常 2：试用 3：欠费 ';
 
-INSERT IGNORE INTO `t_config_server` (`key`, `value`, `tenant_id`, `env_code`, `tenant_app_key`, `is_tenant`, `is_global`, `edition`, `gmt_create`, `gmt_update`, `is_deleted`) VALUES ('takin.asset.balance.default.try', '10000', -99, 'test', 'default', 1, 1, 2, '2021-12-03 18:22:46', NULL, 0);
-INSERT IGNORE INTO `t_config_server` (`key`, `value`, `tenant_id`, `env_code`, `tenant_app_key`, `is_tenant`, `is_global`, `edition`, `gmt_create`, `gmt_update`, `is_deleted`) VALUES ('takin.asset.balance.default.formal', '100000', -99, 'test', 'default', 1, 1, 2, '2021-12-03 18:22:46', NULL, 0);
+INSERT IGNORE INTO `t_config_server` ( `key`, `value`, `tenant_id`, `env_code`, `tenant_app_key`, `is_tenant`, `is_global`, `edition`, `gmt_create`, `gmt_update`, `is_deleted`) VALUES ('takin.asset.balance.default.try', '10000', -99, 'test', 'default', 0, 1, 2, '2021-12-03 18:22:46', '2021-12-20 10:15:52', 0);
+INSERT IGNORE INTO `t_config_server` ( `key`, `value`, `tenant_id`, `env_code`, `tenant_app_key`, `is_tenant`, `is_global`, `edition`, `gmt_create`, `gmt_update`, `is_deleted`) VALUES ('takin.asset.balance.default.formal', '100000', -99, 'test', 'default', 0, 1, 2, '2021-12-03 18:22:46', '2021-12-20 10:15:55', 0);
 INSERT IGNORE INTO `t_config_server` (`key`, `value`, `tenant_id`, `env_code`, `tenant_app_key`, `is_tenant`, `is_global`, `edition`, `gmt_create`, `gmt_update`, `is_deleted`) VALUES ('takin.tenant.default.password', 'shulie@2021', -99, 'test', 'default', 1, 1, 2, '2021-12-03 18:22:46', NULL, 0);
 
 UPDATE `t_tro_user` SET  `is_super`= 1 WHERE name='superadmin';

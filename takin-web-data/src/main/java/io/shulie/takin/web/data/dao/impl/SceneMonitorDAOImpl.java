ca0c621b570ceb46c9fcf8060934fe0a5367f249
liuchuan <zhulifei@shulie.io>
2021-12-29 14:03:29 +0800

    场景监控列表!

---
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/dao/impl/SceneMonitorDAOImpl.java
@@ -0,0 +1,33 @@
+package io.shulie.takin.web.data.dao.impl;
+
+import java.util.List;
+
+import io.shulie.takin.web.common.util.DataTransformUtil;
+import io.shulie.takin.web.data.mapper.mysql.SceneMonitorMapper;
+import io.shulie.takin.web.data.model.mysql.SceneMonitorEntity;
+import io.shulie.takin.web.data.dao.scene.SceneMonitorDAO;
+import io.shulie.takin.web.data.result.SceneMonitorListResult;
+import io.shulie.takin.web.data.util.MPUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 第三方登录服务表(SceneMonitor)表数据库 dao 层实现
+ *
+ * @author liuchuan
+ * @date 2021-12-29 10:20:13
+ */
+@Service
+public class SceneMonitorDAOImpl implements SceneMonitorDAO, MPUtil<SceneMonitorEntity> {
+
+    @Autowired
+    private SceneMonitorMapper sceneMonitorMapper;
+
+    @Override
+    public List<SceneMonitorListResult> listBySceneId(Long sceneId) {
+        return DataTransformUtil.list2list(sceneMonitorMapper.selectList(this.getLambdaQueryWrapper()
+            .select(SceneMonitorEntity::getTitle, SceneMonitorEntity::getUrl)
+            .eq(SceneMonitorEntity::getSceneId, sceneId).last("limit 1000")), SceneMonitorListResult.class);
+    }
+
+}
---
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/SceneMonitorMapper.java
@@ -0,0 +1,15 @@
+package io.shulie.takin.web.data.mapper.mysql;
+
+import io.shulie.takin.web.data.model.mysql.SceneMonitorEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 第三方登录服务表(SceneMonitor)表数据库 mapper
+ *
+ * @author liuchuan
+ * @date 2021-12-29 10:20:12
+ */
+public interface SceneMonitorMapper extends BaseMapper<SceneMonitorEntity> {
+
+}
+

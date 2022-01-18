b6c76f695d174031c56712cf87ec84faf84463e1
nanfeng <knightzhangdev@163.com>
2022-01-18 10:41:53 +0800

    影子库表新增操作日志

--- takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/v2/ds/DsController.java
+++ takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/v2/ds/DsController.java
@@ -3,6 +3,7 @@ package io.shulie.takin.web.entrypoint.controller.v2.ds;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
 import com.pamirs.attach.plugin.dynamic.one.Converter;
 import com.pamirs.attach.plugin.dynamic.one.Type;
 import io.shulie.takin.common.beans.annotation.ActionTypeEnum;
@@ -20,6 +21,7 @@ import io.shulie.takin.web.biz.pojo.response.application.ShadowDetailResponse;
 import io.shulie.takin.web.biz.service.dsManage.DsService;
 import io.shulie.takin.web.common.common.Response;
 import io.shulie.takin.web.common.constant.ApiUrls;
+import io.shulie.takin.web.common.context.OperationLogContextHolder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -90,7 +92,14 @@ public class DsController {
             moduleCode = BizOpConstants.ModuleCode.APPLICATION_MANAGE,
             needAuth = ActionTypeEnum.UPDATE
     )
+    @ModuleDef(
+            moduleName = BizOpConstants.Modules.APPLICATION_MANAGE,
+            subModuleName = BizOpConstants.SubModules.SHADOW_DATABASE_TABLE,
+            logMsgKey = BizOpConstants.Message.MESSAGE_SHADOW_DATABASE_TABLE_UPDATE
+    )
     public Response dsUpdateConfig(@RequestBody @Validated ApplicationDsUpdateInputV2 updateRequestV2) {
+        OperationLogContextHolder.operationType(BizOpConstants.OpTypes.UPDATE);
+        OperationLogContextHolder.addVars(BizOpConstants.Vars.SHADOW_DATABASE_TABLE_URL,updateRequestV2.getUrl());
         return dsService.dsUpdateConfig(updateRequestV2);
     }
 
@@ -112,7 +121,14 @@ public class DsController {
             moduleCode = BizOpConstants.ModuleCode.APPLICATION_MANAGE,
             needAuth = ActionTypeEnum.DELETE
     )
+    @ModuleDef(
+            moduleName = BizOpConstants.Modules.APPLICATION_MANAGE,
+            subModuleName = BizOpConstants.SubModules.SHADOW_DATABASE_TABLE,
+            logMsgKey = BizOpConstants.Message.MESSAGE_SHADOW_DATABASE_TABLE_DELETE
+    )
     public Response  dsDeleteDsConfig(@RequestBody @Validated ApplicationDsDeleteInputV2 inputV2) {
+        OperationLogContextHolder.operationType(BizOpConstants.OpTypes.DELETE);
+        OperationLogContextHolder.addVars(BizOpConstants.Vars.SHADOW_DATABASE_TABLE_JSON, JSON.toJSONString(inputV2));
         dsService.dsDeleteV2(inputV2.getId(),inputV2.getMiddlewareType(),inputV2.getIsNewData(),inputV2.getApplicationId());
         return Response.success();
     }
@@ -123,7 +139,14 @@ public class DsController {
             moduleCode = BizOpConstants.ModuleCode.APPLICATION_MANAGE,
             needAuth = ActionTypeEnum.CREATE
     )
+    @ModuleDef(
+            moduleName = BizOpConstants.Modules.APPLICATION_MANAGE,
+            subModuleName = BizOpConstants.SubModules.SHADOW_DATABASE_TABLE,
+            logMsgKey = BizOpConstants.Message.MESSAGE_SHADOW_DATABASE_TABLE_CREATE
+    )
     public Response dsCreateConfig(@RequestBody @Validated ApplicationDsCreateInputV2 createRequestV2) {
+        OperationLogContextHolder.operationType(BizOpConstants.OpTypes.CREATE);
+        OperationLogContextHolder.addVars(BizOpConstants.Vars.SHADOW_DATABASE_TABLE_JSON, JSON.toJSONString(createRequestV2));
         return dsService.dsCreateConfig(createRequestV2);
     }
 
@@ -179,6 +202,10 @@ public class DsController {
             needAuth = ActionTypeEnum.ENABLE_DISABLE
     )
     public Response enableConfig(@Validated @RequestBody ApplicationDsEnableInputV2 enableRequest) {
+        OperationLogContextHolder.operationType(
+                Integer.valueOf(0).equals(enableRequest.getStatus()) ? BizOpConstants.OpTypes.ENABLE
+                        : BizOpConstants.OpTypes.DISABLE);
+        OperationLogContextHolder.addVars(BizOpConstants.Vars.SHADOW_DATABASE_TABLE_JSON, JSON.toJSONString(enableRequest));
         return dsService.enableConfigV2( enableRequest.getId(),enableRequest.getMiddlewareType(),
                 enableRequest.getIsNewData(),enableRequest.getApplicationId(),enableRequest.getStatus());
     }

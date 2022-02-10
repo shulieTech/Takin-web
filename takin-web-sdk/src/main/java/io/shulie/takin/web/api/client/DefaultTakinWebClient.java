260d6b29b9d0363b4d19bf89dfcc2313d96d3152
caijianying <caijianying@shulie.io>
2022-02-10 16:48:00 +0800

    优化

--- takin-web-sdk/src/main/java/io/shulie/takin/web/api/client/DefaultTakinWebClient.java
+++ takin-web-sdk/src/main/java/io/shulie/takin/web/api/client/DefaultTakinWebClient.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
  * @author caijianying
  */
 @Slf4j
-public class DefaultTakinWebClient extends AbstractTakinWebClient{
+public class DefaultTakinWebClient extends AbstractTakinWebClient {
 
     public DefaultTakinWebClient() {
 
@@ -24,22 +24,25 @@ public class DefaultTakinWebClient extends AbstractTakinWebClient{
     }
 
     @Override
-    public String execute(TakinWebRequest webRequest){
-        if (this.webUrl == null || this.appId == null || this.appSecret == null){
-            throw new RuntimeException("缺少必要参数！webUrl、appId、appSecret");
+    public String execute(TakinWebRequest webRequest) {
+        if (this.webUrl == null || this.appId == null || this.appSecret == null) {
+            throw new IllegalArgumentException("缺少必要参数！webUrl、appId、appSecret");
+        }
+        if (webRequest == null || webRequest.getServerUrl() == null || webRequest.getRequestData() == null || webRequest.getMethod() == null) {
+            throw new IllegalArgumentException("缺少必要参数！serverUrl、requestData、method");
         }
         final Map headerMap = getCallerHeader();
         final Method method = webRequest.getMethod();
         String responseJson = null;
-        if (method.equals(Method.GET)){
-            responseJson = HttpClientUtil.sendGet(webRequest.getServerUrl(), webRequest.getRequestData(),headerMap);
-        }
-        if (method.equals(Method.POST)){
-            responseJson = HttpClientUtil.sendPost(webRequest.getServerUrl(), webRequest.getRequestData(),headerMap);
+        if (method.equals(Method.GET)) {
+            responseJson = HttpClientUtil.sendGet(webRequest.getServerUrl(), webRequest.getRequestData(), headerMap);
+        } else if (method.equals(Method.POST)) {
+            responseJson = HttpClientUtil.sendPost(webRequest.getServerUrl(), webRequest.getRequestData(), headerMap);
+        } else {
+            throw new UnsupportedOperationException("http method【" + method + "】 is not supported!");
         }
 
         return responseJson;
     }
-
 
 }

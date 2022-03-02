d15f7fff35ceca7d06d699671b9fb08933bcfd05
张天赐 <zhangtianci@shulie.io>
2022-03-02 18:27:39 +0800

    fix:脚本校验(区分版本)
    前端请求默认值-更新脚本

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/pojo/request/scriptmanage/ScriptManageDeployUpdateRequest.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/pojo/request/scriptmanage/ScriptManageDeployUpdateRequest.java
@@ -46,7 +46,7 @@ public class ScriptManageDeployUpdateRequest implements Serializable {
     /**
      * 脚本版本
      */
-    private Integer mVersion;
+    private Integer mVersion = 0;
 
     /**
      * 关联文件列表

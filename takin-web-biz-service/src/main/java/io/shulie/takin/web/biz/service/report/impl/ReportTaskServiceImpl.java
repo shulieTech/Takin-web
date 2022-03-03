f04bed2360465829d64d07095d4da54f7b2a3c7b
何仲奇 <576425673@qq.com>
2022-03-03 16:55:17 +0800

    bug修改

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/report/impl/ReportTaskServiceImpl.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/report/impl/ReportTaskServiceImpl.java
@@ -22,7 +22,6 @@ import io.shulie.takin.web.biz.utils.job.JobRedisUtils;
 import io.shulie.takin.web.common.common.Separator;
 import io.shulie.takin.web.common.pojo.dto.SceneTaskDto;
 import io.shulie.takin.web.common.util.CommonUtil;
-import io.shulie.takin.web.common.util.JsonUtil;
 import io.shulie.takin.web.common.util.SceneTaskUtils;
 import io.shulie.takin.web.data.dao.leakverify.LeakVerifyResultDAO;
 import io.shulie.takin.web.diff.api.scenetask.SceneTaskApi;
@@ -223,8 +222,6 @@ public class ReportTaskServiceImpl implements ReportTaskService {
                 return;
             }
             WebPluginUtils.setTraceTenantContext(commonExt);
-
-            log.info("traceTenantContext:{}", JsonUtil.bean2Json(WebPluginUtils.traceTenantCommonExt()));
 
             try {
                 // 检查风险机器

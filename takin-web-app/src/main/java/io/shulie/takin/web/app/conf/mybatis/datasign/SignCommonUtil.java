2ff5b3e497f58c2b2da64eb830f31e07bd498941
nanfeng <knightzhangdev@163.com>
2022-03-15 09:40:18 +0800

    修改签名bug

--- takin-web-app/src/main/java/io/shulie/takin/web/app/conf/mybatis/datasign/SignCommonUtil.java
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/conf/mybatis/datasign/SignCommonUtil.java
@@ -134,7 +134,7 @@ public class SignCommonUtil {
                 String whereStr = " where" + sql.split("WHERE")[1];
                 String querySql = "select * from " + tableName + whereStr;
 
-                ResultSet rs = statement.getConnection().createStatement().executeQuery(querySql);
+                ResultSet rs = statement.executeQuery(querySql);
                 ResultSetMetaData md = rs.getMetaData();
                 Map<String, Object> map = new HashMap<>();
                 List<String> sqlList = new ArrayList<>();

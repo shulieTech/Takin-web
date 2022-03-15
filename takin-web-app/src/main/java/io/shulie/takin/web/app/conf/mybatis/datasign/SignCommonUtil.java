68b9e31fea479185f5d0cbcc3c1d9f1a201fc174
nanfeng <knightzhangdev@163.com>
2022-03-15 09:39:25 +0800

    修改签名插件报错问题

--- takin-web-app/src/main/java/io/shulie/takin/web/app/conf/mybatis/datasign/SignCommonUtil.java
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/conf/mybatis/datasign/SignCommonUtil.java
@@ -73,7 +73,7 @@ public class SignCommonUtil {
                 String whereStr = " where id = " + id;
                 String querySql = "select * from " + tableName + whereStr;
 
-                ResultSet rs = statement.executeQuery(querySql);
+                ResultSet rs = statement.getConnection().createStatement().executeQuery(querySql);
                 ResultSetMetaData md = rs.getMetaData();
                 Map<String, Object> map = new HashMap<>();
                 while (rs.next()) {
@@ -133,7 +133,8 @@ public class SignCommonUtil {
                 String tableName = update.getTable().getName();
                 String whereStr = " where" + sql.split("WHERE")[1];
                 String querySql = "select * from " + tableName + whereStr;
-                ResultSet rs = statement.executeQuery(querySql);
+
+                ResultSet rs = statement.getConnection().createStatement().executeQuery(querySql);
                 ResultSetMetaData md = rs.getMetaData();
                 Map<String, Object> map = new HashMap<>();
                 List<String> sqlList = new ArrayList<>();

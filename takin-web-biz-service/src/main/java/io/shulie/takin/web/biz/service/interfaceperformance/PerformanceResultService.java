69581802171428fc9da42bb0741084144d255076
zhouyuan <zhouyuan@shulie.io>
2022-05-26 14:28:29 +0800

    接口压测

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/interfaceperformance/PerformanceResultService.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/interfaceperformance/PerformanceResultService.java
@@ -10,7 +10,25 @@ import io.shulie.takin.web.biz.pojo.request.interfaceperformance.PerformanceResu
  * @date 2022/5/20 10:20 上午
  */
 public interface PerformanceResultService {
+    /**
+     * 新增结果
+     *
+     * @param resultCreateInput
+     */
     void add(PerformanceResultCreateInput resultCreateInput);
 
+    /**
+     * 分页获取结果
+     *
+     * @param param
+     * @return
+     */
     PagingList<PerformanceResultResponse> pageResult(PerformanceResultCreateInput param);
+
+    /**
+     * 清理结果
+     *
+     * @param param
+     */
+    void flushAll(PerformanceResultCreateInput param);
 }

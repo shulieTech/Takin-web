cd841dabafb84aab2fa545992d15aec1d69fea0a
zhouyuan <zhouyuan@shulie.io>
2022-05-31 10:40:16 +0800

    支持多类参数

--- takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/interfaceperformance/PerformanceConfigController.java
+++ takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/interfaceperformance/PerformanceConfigController.java
@@ -47,7 +47,7 @@ public class PerformanceConfigController {
 
     @ApiOperation("单接口压测删除")
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-    public ResponseResult delete(PerformanceConfigQueryRequest request) {
+    public ResponseResult delete(@RequestBody PerformanceConfigQueryRequest request) {
         if (request == null) {
             throw new TakinWebException(TakinWebExceptionEnum.INTERFACE_PERFORMANCE_PARAM_ERROR, "参数为空");
         }

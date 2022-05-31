71bda3ac0768bc7d15a4b00d7c5b759999535bb3
zhangjunjie <zhangjunjie@shulie.io>
2022-05-31 13:53:03 +0800

    跨域

--- takin-web-app/src/main/java/io/shulie/takin/web/app/ApplicationFilter.java
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/ApplicationFilter.java
@@ -5,10 +5,14 @@ import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.TypeFilter;
 import org.springframework.lang.NonNull;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author 张天赐
  */
-public class ApplicationFilter implements TypeFilter {
+public class ApplicationFilter implements TypeFilter, Filter {
     /**
      * Determine whether this filter matches for the class described by
      * the given metadata.
@@ -27,5 +31,25 @@ public class ApplicationFilter implements TypeFilter {
         }
         // 正常启动.
         else {return false;}
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age","3600");
+        response.setHeader("Access-Control-Allow-Headers","x-requested-with,content-type");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }

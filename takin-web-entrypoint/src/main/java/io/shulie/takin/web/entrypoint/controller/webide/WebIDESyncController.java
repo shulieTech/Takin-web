460755726cbd418106d5adf350790754c672f2ec
nanfeng <knightzhangdev@163.com>
2022-06-22 13:41:13 +0800

    get请求修改成post请求

--- takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/webide/WebIDESyncController.java
+++ takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/webide/WebIDESyncController.java
@@ -5,6 +5,7 @@ import io.shulie.takin.web.biz.service.webide.WebIDESyncService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class WebIDESyncController {
     private WebIDESyncService webIDESyncService;
 
     @ApiOperation("同步业务流程并启动调试")
-    @GetMapping("/script/businessFlow/sync")
+    @PostMapping("/script/businessFlow/sync")
     public void syncScriptBusinessFlow(@RequestBody WebIDESyncScriptRequest request){
         if(Objects.nonNull(request)){
             webIDESyncService.syncScript(request);

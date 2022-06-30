a061aa1564b1f2f58c64afd9ac8852a66b32045e
zhouyuan <zhouyuan@shulie.io>
2022-06-30 14:20:23 +0800

    业务流程-文件操作的时候，如果有问题，快速失败掉,提示异常信息

--- takin-web-service-adapters/takin-web-diff/takin-web-diff-cloud/src/main/java/io/shulie/takin/web/diff/cloud/impl/DiffFileApiImpl.java
+++ takin-web-service-adapters/takin-web-diff/takin-web-diff-cloud/src/main/java/io/shulie/takin/web/diff/cloud/impl/DiffFileApiImpl.java
@@ -1,5 +1,7 @@
 package io.shulie.takin.web.diff.cloud.impl;
 
+import java.io.File;
+import java.util.List;
 import java.util.Map;
 
 import io.shulie.takin.cloud.entrypoint.file.CloudFileApi;
@@ -10,9 +12,13 @@ import io.shulie.takin.cloud.sdk.model.request.filemanager.FileDeleteParamReq;
 import io.shulie.takin.cloud.sdk.model.request.filemanager.FileZipParamReq;
 import io.shulie.takin.common.beans.response.ResponseResult;
 import io.shulie.takin.utils.string.StringUtil;
+import io.shulie.takin.web.common.exception.TakinWebException;
+import io.shulie.takin.web.common.exception.TakinWebExceptionEnum;
 import io.shulie.takin.web.diff.api.DiffFileApi;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -25,6 +31,9 @@ import org.springframework.util.CollectionUtils;
 public class DiffFileApiImpl implements DiffFileApi {
     @Autowired
     private CloudFileApi cloudFileApi;
+
+    @Value("${file.upload.openFileValid:true}")
+    private boolean openFileValid;
 
     @Override
 
@@ -51,9 +60,18 @@ public class DiffFileApiImpl implements DiffFileApi {
         if (CollectionUtils.isEmpty(req.getSourcePaths())
             || StringUtil.isBlank(req.getTargetPath())) {return true;}
         try {
-            return cloudFileApi.copyFile(req);
+            //return cloudFileApi.copyFile(req);
+            if (!this.isExist(req.getSourcePaths())) {
+                throw new TakinWebException(TakinWebExceptionEnum.SCRIPT_VALIDATE_ERROR, "关联的文件已丢失,请检查!!!");
+            }
+            if (!cloudFileApi.copyFile(req)) {
+                throw new TakinWebException(TakinWebExceptionEnum.SCRIPT_VALIDATE_ERROR, "文件操作失败,请重试！");
+            }
+            return true;
         } catch (Exception e) {
-            return false;
+            log.error("copyFile is fail " + ExceptionUtils.getStackTrace(e));
+            // 这里直接抛异常处理,不能吃掉异常信息
+            throw e;
         }
     }
 
@@ -78,4 +96,32 @@ public class DiffFileApiImpl implements DiffFileApi {
         return cloudFileApi.createFileByPathAndString(req);
     }
 
+    /**
+     * 批量验证文件
+     *
+     * @param pathList
+     * @return
+     */
+    @Override
+    public boolean isExist(List<String> pathList) {
+        if (!openFileValid) {
+            // 关闭的时候直接返回成功
+            return true;
+        }
+        if (CollectionUtils.isEmpty(pathList)) {
+            return false;
+        }
+        boolean isExist = true;
+        for (int i = 0; i < pathList.size(); i++) {
+            File file = new File(pathList.get(i));
+            if (file != null && file.isFile()) {
+                // 继续验证下一个
+            } else {
+                isExist = false;
+                log.error("复制的文件不存在,请检查" + pathList.get(i));
+                break;
+            }
+        }
+        return isExist;
+    }
 }

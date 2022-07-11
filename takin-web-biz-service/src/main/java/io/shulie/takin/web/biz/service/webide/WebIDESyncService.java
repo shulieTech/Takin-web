0b6ea6434b72f0d7208cb0431c55542e9b514b3b
张正 <zz052831@163.com>
2022-07-11 16:08:15 +0800

    Revert "新增webIDE支持接口"

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/webide/WebIDESyncService.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/webide/WebIDESyncService.java
@@ -1,12 +1,6 @@
 package io.shulie.takin.web.biz.service.webide;
 
-import io.shulie.takin.common.beans.page.PagingList;
-import io.shulie.takin.web.biz.pojo.request.linkmanage.BusinessFlowPageQueryRequest;
 import io.shulie.takin.web.biz.pojo.request.webide.WebIDESyncScriptRequest;
-import io.shulie.takin.web.biz.pojo.response.linkmanage.BusinessActivityInfoResponse;
-import io.shulie.takin.web.biz.pojo.response.linkmanage.BusinessFlowListWebIDEResponse;
-
-import java.util.List;
 
 /**
  * @Author: 南风
@@ -15,8 +9,4 @@ import java.util.List;
 public interface WebIDESyncService {
 
     void syncScript(WebIDESyncScriptRequest request);
-
-    PagingList<BusinessFlowListWebIDEResponse> sceneList(BusinessFlowPageQueryRequest queryRequest);
-
-    List<BusinessActivityInfoResponse> activityList(Long businessFlowId);
 }
--- takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/webide/WebIDESyncController.java
+++ takin-web-entrypoint/src/main/java/io/shulie/takin/web/entrypoint/controller/webide/WebIDESyncController.java
@@ -1,24 +1,16 @@
 package io.shulie.takin.web.entrypoint.controller.webide;
 
-import io.shulie.takin.common.beans.page.PagingList;
-import io.shulie.takin.web.biz.pojo.request.linkmanage.BusinessFlowPageQueryRequest;
 import io.shulie.takin.web.biz.pojo.request.webide.WebIDESyncScriptRequest;
-import io.shulie.takin.web.biz.pojo.response.linkmanage.BusinessActivityInfoResponse;
-import io.shulie.takin.web.biz.pojo.response.linkmanage.BusinessFlowListWebIDEResponse;
 import io.shulie.takin.web.biz.service.webide.WebIDESyncService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -33,32 +25,11 @@ public class WebIDESyncController {
     @Resource
     private WebIDESyncService webIDESyncService;
 
-
-
     @ApiOperation("同步业务流程并启动调试")
     @PostMapping("/script/businessFlow/sync")
     public void syncScriptBusinessFlow(@RequestBody WebIDESyncScriptRequest request){
         if(Objects.nonNull(request)){
             webIDESyncService.syncScript(request);
         }
-    }
-
-    @ApiOperation("获取业务流程列表")
-    @GetMapping("/script/businessFlow/sceneList")
-    public PagingList<BusinessFlowListWebIDEResponse> sceneList(@ApiParam("业务流程名称") String businessFlowName, Integer current, Integer pageSize){
-        BusinessFlowPageQueryRequest queryRequest = new BusinessFlowPageQueryRequest();
-        queryRequest.setCurrentPage(current);
-        queryRequest.setPageSize(pageSize);
-        queryRequest.setBusinessFlowName(businessFlowName);
-        return webIDESyncService.sceneList(queryRequest);
-    }
-
-    @ApiOperation("获取业务流程下的业务活动列表")
-    @GetMapping("/script/businessFlow/activityList")
-    public List<BusinessActivityInfoResponse> activityList(@RequestParam("businessFlowId") Long businessFlowId){
-        if(Objects.isNull(businessFlowId)){
-            return new ArrayList<>();
-        }
-        return webIDESyncService.activityList(businessFlowId);
     }
 }

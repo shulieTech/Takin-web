6645353ee540d9eb6db8cb822a12e080f40cf12a
zhouyuan <zhouyuan@shulie.io>
2022-07-13 11:30:31 +0800

    远程调用拆分单独job和分片处理

--- takin-web-app/src/main/java/io/shulie/takin/web/app/conf/ThreadPoolConfig.java
+++ takin-web-app/src/main/java/io/shulie/takin/web/app/conf/ThreadPoolConfig.java
@@ -60,6 +60,18 @@ public class ThreadPoolConfig {
     }
 
     /**
+     * 远程调用job
+     *
+     * @return
+     */
+    @Bean(name = "remoteApiThreadPool")
+    public ThreadPoolExecutor remoteApiThreadPool() {
+        ThreadFactory nameThreadFactory = new ThreadFactoryBuilder().setNameFormat("remoteApi-job-%d").build();
+        return new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<>(5000), nameThreadFactory,
+                new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    /**
      * 方法追踪超时检测
      *
      * @return

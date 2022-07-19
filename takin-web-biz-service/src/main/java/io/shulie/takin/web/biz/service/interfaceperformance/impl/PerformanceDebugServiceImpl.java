a6dee5ceeb755488fba7462b0bbb13cd3a67f84a
zhouyuan <zhouyuan@shulie.io>
2022-07-19 17:05:57 +0800

    在线编辑需求5.8.2

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/interfaceperformance/impl/PerformanceDebugServiceImpl.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/interfaceperformance/impl/PerformanceDebugServiceImpl.java
@@ -209,7 +209,7 @@ public class PerformanceDebugServiceImpl implements PerformanceDebugService {
                 // 保存请求结果
                 performanceResultService.add(insertResult);
             } else {
-                redisClientUtil.setString(uuId, "", 480, TimeUnit.SECONDS);
+                redisClientUtil.setString(performanceDebugUtil.formatResultKey(uuId), "1", 480, TimeUnit.SECONDS);
                 // 异步转换下脚本调试结果和原来的takin压测结果
                 CompletableFuture.runAsync(() -> convertDebugResult(
                         uuId,
@@ -302,7 +302,7 @@ public class PerformanceDebugServiceImpl implements PerformanceDebugService {
         } catch (Throwable e) {
             log.error("获取结果失败" + ExceptionUtils.getStackTrace(e));
         } finally {
-            redisClientUtil.delete(resultId);
+            redisClientUtil.delete(performanceDebugUtil.formatResultKey(resultId));
         }
     }
 

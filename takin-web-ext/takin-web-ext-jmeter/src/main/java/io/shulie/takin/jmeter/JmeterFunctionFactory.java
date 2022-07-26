2b4c694336396f6fc2998c44cc39f1480067d650
zhouyuan <zhouyuan@shulie.io>
2022-07-26 11:47:14 +0800

    在线编辑需求5.8.2

--- takin-web-ext/takin-web-ext-jmeter/src/main/java/io/shulie/takin/jmeter/JmeterFunctionFactory.java
+++ takin-web-ext/takin-web-ext-jmeter/src/main/java/io/shulie/takin/jmeter/JmeterFunctionFactory.java
@@ -55,52 +55,36 @@ public class JmeterFunctionFactory {
      * 有变量的，暂时没处理，业务需求不一定能用到
      */
     static {
-        // 随机数
-        functionMap.put("__RandomString", new RandomString());
-        functionMap.put("__Random", new Random());
-        functionMap.put("__RandomDate", new RandomDate());
-
-        functionMap.put("__machineIP", new MachineIP());
-        functionMap.put("__machineName", new MachineName());
-
-        functionMap.put("__time", new TimeFunction());
-        functionMap.put("__timeShift", new TimeShift());
-        functionMap.put("__dateTimeConvert", new DateTimeConvertFunction());
-
-        functionMap.put("__UUID", new Uuid());
-
-        //functionMap.put("__groovy", new Groovy());
-        //functionMap.put("__javaScript", new JavaScript());
-        functionMap.put("__BeanShell", new BeanShell());
-
-        // 返回当前线程号,从1开始递增。
+        // 获取信息函数
         functionMap.put("__threadNum", new ThreadNumber());
         functionMap.put("__threadGroupName", new ThreadGroupName());
         functionMap.put("__samplerName", new SamplerName());
         functionMap.put("__TestPlanName", new TestPlanName());
+        functionMap.put("__time", new TimeFunction());
+        functionMap.put("__timeShift", new TimeShift());
+        functionMap.put("__machineIP", new MachineIP());
+        functionMap.put("__machineName", new MachineName());
 
-        // 统计线程的迭代次数
+        // 数据计算函数
         functionMap.put("__counter", new IterationCounter());
+        functionMap.put("__RandomString", new RandomString());
+        functionMap.put("__Random", new Random());
+        functionMap.put("__RandomDate", new RandomDate());
+        functionMap.put("__UUID", new Uuid());
+        functionMap.put("__intSum", new IntSum());
+        functionMap.put("__longSum", new LongSum());
+        functionMap.put("__dateTimeConvert", new DateTimeConvertFunction());
 
-        // 动态设置Jmeter属性
-        //functionMap.put("__setProperty", new SetProperty());
-        // 获取jmeter.properties文件中设置的JMeter属性。
-        //functionMap.put("__property", new Property());
-        // 获取命令行中定义的属性
-        //functionMap.put("__P", new Property2());
-        // 执行变量表达式并返回结果，如果需要嵌套的使用变量时
-        //functionMap.put("__V", new Variable());
+        // 脚本函数
+        //functionMap.put("__groovy", new Groovy());
+        //functionMap.put("__javaScript", new JavaScript());
+        //functionMap.put("__BeanShell", new BeanShell());
+
         // 把数字转化成Unicode字符。
         functionMap.put("__char", new CharFunction());
 
-        //int型求和
-        functionMap.put("__intSum", new IntSum());
-        functionMap.put("__longSum", new LongSum());
-
         //计算表达式。
         functionMap.put("__eval", new EvalFunction());
-        // 把表达式的结果存入变量。
-        //functionMap.put("__evalVar", new EvalVarFunction());
     }
 
     public static AbstractFunction getAbstractFunction(String key) {

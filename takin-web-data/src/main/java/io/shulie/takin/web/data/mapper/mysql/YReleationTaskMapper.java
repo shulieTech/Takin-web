d4b74e673f2e9c4bfe4ff132454d829cdfba61cb
zhangjunjie <zhangjunjie@shulie.io>
2022-08-01 17:02:53 +0800

    鉴权

---
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/YReleationTaskMapper.java
@@ -0,0 +1,10 @@
+package io.shulie.takin.web.data.mapper.mysql;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.shulie.takin.web.data.model.mysql.YReleationTaskEntity;
+import io.shulie.takin.web.data.model.mysql.YVersionEntity;
+
+public interface YReleationTaskMapper extends BaseMapper<YReleationTaskEntity> {
+
+}
---
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/model/mysql/YReleationTaskEntity.java
@@ -0,0 +1,30 @@
+package io.shulie.takin.web.data.model.mysql;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.shulie.takin.web.data.model.mysql.base.TenantBaseEntity;
+import lombok.Data;
+
+@Data
+@TableName(value = "y_releation_task")
+public class YReleationTaskEntity extends TenantBaseEntity {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 文件id
+     */
+    @TableField(value = "task_id")
+    private Long taskId;
+
+    /**
+     * 脚本id
+     */
+    @TableField(value = "env_code")
+    private String envCode;
+
+    @TableField(value = "user_id")
+    private String userId;
+}

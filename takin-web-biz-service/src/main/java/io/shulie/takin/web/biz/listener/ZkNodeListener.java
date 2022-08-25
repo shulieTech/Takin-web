2c9695132ebcc8aff65a9b4fb23f96fb21b58481
zhaoyong <zhaoyong@shulie.io>
2022-08-25 18:20:00 +0800

    zk监听

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/listener/ZkNodeListener.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/listener/ZkNodeListener.java
@@ -1,19 +1,14 @@
 package io.shulie.takin.web.biz.listener;
 
-import com.pamirs.takin.entity.domain.query.ApplicationQueryRequest;
-import com.pamirs.takin.entity.domain.vo.ApplicationVo;
 import io.shulie.takin.utils.json.JsonHelper;
-import io.shulie.takin.web.amdb.bean.result.application.ApplicationNodeDTO;
 import io.shulie.takin.web.amdb.bean.result.application.ApplicationNodeZkDTO;
 import io.shulie.takin.web.biz.service.ApplicationService;
 import io.shulie.takin.web.biz.service.agentupgradeonline.AgentReportService;
 import io.shulie.takin.web.biz.utils.AgentZkClientUtil;
-import io.shulie.takin.web.common.common.Response;
 import io.shulie.takin.web.common.enums.config.ConfigServerKeyEnum;
 import io.shulie.takin.web.common.enums.fastagentaccess.AgentReportStatusEnum;
 import io.shulie.takin.web.data.param.agentupgradeonline.CreateAgentReportParam;
 import io.shulie.takin.web.data.util.ConfigServerHelper;
-import javafx.fxml.Initializable;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.curator.framework.CuratorFramework;
@@ -21,13 +16,10 @@ import org.apache.curator.framework.recipes.cache.TreeCache;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
 import org.apache.curator.framework.recipes.cache.TreeCacheListener;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.net.URL;
 import java.util.List;
-import java.util.ResourceBundle;
 
 /**
  * 监听zk节点变化，目前用来兼容agent1.0的应用状态变化
@@ -49,25 +41,33 @@ public class ZkNodeListener implements InitializingBean {
             CuratorFramework client = this.client.getClient();
             String agentRegisteredPath = ConfigServerHelper.getValueByKey(ConfigServerKeyEnum.AGENT_REGISTERED_PATH);
             log.info("zk监听的节点为:" + agentRegisteredPath);
+            if (agentRegisteredPath == null) {
+                log.warn("zk节点信息不存在，本次不会做节点监听，如果需要兼容agent1.0版本请在数据库添加zk节点");
+                return;
+            }
+            if (agentRegisteredPath.endsWith("/")) {
+                agentRegisteredPath = agentRegisteredPath.substring(0, agentRegisteredPath.length() - 1);
+            }
             TreeCache treeCache = new TreeCache(client, agentRegisteredPath);
+            String finalAgentRegisteredPath = agentRegisteredPath;
             TreeCacheListener treeCacheListener = new TreeCacheListener() {
                 @Override
                 public void childEvent(CuratorFramework curatorFramework, TreeCacheEvent treeCacheEvent) throws Exception {
-                    log.info(treeCacheEvent.getType()+"==>"+treeCacheEvent.getData().getPath());
-                    List<String> applicationNames = client.getChildren().forPath(agentRegisteredPath);
+                    log.info(treeCacheEvent.getType() + "==>" + treeCacheEvent.getData().getPath());
+                    List<String> applicationNames = client.getChildren().forPath(finalAgentRegisteredPath);
                     log.info("获取到当前所有的子节点为:" + applicationNames);
-                    if (CollectionUtils.isNotEmpty(applicationNames)){
-                        for (String application : applicationNames){
+                    if (CollectionUtils.isNotEmpty(applicationNames)) {
+                        for (String application : applicationNames) {
                             List<String> agentIds = client.getChildren().forPath(application);
-                            if (CollectionUtils.isEmpty(agentIds)){
+                            if (CollectionUtils.isEmpty(agentIds)) {
                                 continue;
                             }
                             String applicationName = application.substring(application.lastIndexOf("/"));
                             Long appId = applicationService.queryApplicationIdByAppName(applicationName);
-                            if (appId == null){
+                            if (appId == null) {
                                 continue;
                             }
-                            for (String agentId : agentIds){
+                            for (String agentId : agentIds) {
                                 byte[] bytes = client.getData().forPath(agentId);
                                 String s = new String(bytes);
                                 ApplicationNodeZkDTO applicationNodeDTO = JsonHelper.json2Bean(s, ApplicationNodeZkDTO.class);
@@ -92,7 +92,7 @@ public class ZkNodeListener implements InitializingBean {
             treeCache.getListenable().addListener(treeCacheListener);
             treeCache.start();
         } catch (Exception e) {
-            log.error("监听zk节点出现异常");
+            log.error("监听zk节点出现异常", e);
         }
     }
 }

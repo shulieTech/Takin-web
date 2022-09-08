96f0d54d8451093e6422a05d43fb84f99e3cd85a
zhouyuan <zhouyuan@shulie.io>
2022-09-08 15:16:45 +0800

    压测资源自动梳理

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/pressureresource/impl/PressureResourceCommonServiceImpl.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/pressureresource/impl/PressureResourceCommonServiceImpl.java
@@ -37,7 +37,6 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;

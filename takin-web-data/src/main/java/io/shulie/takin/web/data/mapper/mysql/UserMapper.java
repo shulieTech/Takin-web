e2a1ba3da96a859afbff7ee694f63827ccfa718b
zhangjunjie <zhangjunjie@shulie.io>
2022-09-19 15:51:36 +0800

    压测统计

--- takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/UserMapper.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/UserMapper.java
@@ -4,6 +4,4 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserMapper {
     Long getIdByName(@Param("userName") String userName);
-
-    String getNameById(@Param("id") Long id);
 }

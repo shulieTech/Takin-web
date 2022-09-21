6b2ce46eafd26e323734d60be0aaff36f5646273
zhouyuan <zhouyuan@shulie.io>
2022-09-21 16:36:05 +0800

    压测资源准备

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/pressureresource/common/IsolateTypeEnum.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/service/pressureresource/common/IsolateTypeEnum.java
@@ -8,7 +8,7 @@ package io.shulie.takin.web.biz.service.pressureresource.common;
 public enum IsolateTypeEnum {
     DEFAULT(0, "无"),
     SHADOW_DB(1, "影子库"),
-    SHADOW_DB_TABLE(2, "影子库/影子表"),
+    SHADOW_DB_TABLE(2, "影子库-影子表"),
     SHADOW_TABLE(3, "影子表");
 
     int code;

b89ead593a270743db0ea1d91ea6443c80a60e02
zhouyuan <zhouyuan@shulie.io>
2022-10-21 10:20:10 +0800

    并行处理报错

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/job/PressureResourceChangeJob.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/job/PressureResourceChangeJob.java
@@ -74,9 +74,9 @@ public class PressureResourceChangeJob implements SimpleJob {
                     }
                     ResourceContextUtil.setTenantContext(resource);
                     pressureResourceCommandService.pushCommand(taskVo);
+                } finally {
                     // 移除Redis数据
                     pressureResourceCommonService.deleteCommandTask(taskVo);
-                } finally {
                     distributedLock.unLockSafely(lockKey);
                 }
             });

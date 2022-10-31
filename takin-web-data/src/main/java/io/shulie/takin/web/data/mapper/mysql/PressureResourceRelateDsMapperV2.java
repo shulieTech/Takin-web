ad9ea54ee26f594154b67c3f2b60d61c07eebb3d
zhouyuan <zhouyuan@shulie.io>
2022-10-31 18:24:12 +0800

    压测准备-兼容版本

--- takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/PressureResourceRelateDsMapperV2.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/mapper/mysql/PressureResourceRelateDsMapperV2.java
@@ -12,9 +12,9 @@ public interface PressureResourceRelateDsMapperV2
     @InterceptorIgnore(tenantLine = "true")
     @Insert("<script>" +
             "insert ignore into t_pressure_resource_relate_ds_v2(" +
-            "resource_id,detail_id,app_name,business_database,relate_id,type,tenant_id,env_code,gmt_create)" +
+            "resource_id,detail_id,app_name,business_database,business_user_name,relate_id,type,tenant_id,env_code,gmt_create)" +
             "values " +
-            "(#{item.resourceId},#{item.detailId},#{item.appName},#{item.businessDatabase},#{item.relateId},#{item.type}," +
+            "(#{item.resourceId},#{item.detailId},#{item.appName},#{item.businessDatabase},#{item.businessUserName},#{item.relateId},#{item.type}," +
             "#{item.tenantId},#{item.envCode},#{item.gmtCreate})" +
             "</script>")
     void saveOrUpdate(@Param("item") PressureResourceRelateDsEntityV2 item);

36a36adc8ab63b7897f06e27b07abf35b09cf8a0
zhouyuan <zhouyuan@shulie.io>
2022-11-01 19:19:08 +0800

    压测准备-兼容版本

--- takin-web-data/src/main/java/io/shulie/takin/web/data/dao/pressureresource/impl/RelateConvert.java
+++ takin-web-data/src/main/java/io/shulie/takin/web/data/dao/pressureresource/impl/RelateConvert.java
@@ -1,8 +1,12 @@
 package io.shulie.takin.web.data.dao.pressureresource.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.pamirs.attach.plugin.dynamic.one.Type;
+import com.pamirs.takin.common.enums.ds.DsTypeEnum;
 import io.shulie.takin.web.data.model.mysql.pressureresource.RelateDsEntity;
 import io.shulie.takin.web.data.result.application.ApplicationDsDbManageDetailResult;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author xingchen
@@ -13,15 +17,28 @@ public class RelateConvert {
     /**
      * 应用详情关联数据源转换为压测资源准备数据源信息
      *
-     * @param result
+     * @param convert
      * @return
      */
-    public static RelateDsEntity dsManageConvertRelateDs(ApplicationDsDbManageDetailResult result) {
+    public static RelateDsEntity dsManageConvertRelateDs(ApplicationDsDbManageDetailResult convert) {
         final RelateDsEntity dsEntity = new RelateDsEntity();
-        dsEntity.setAppName(result.getApplicationName());
-        dsEntity.setBusinessUserName(result.getUserName());
-        dsEntity.setBusinessDatabase(result.getUrl());
-        dsEntity.setMiddlewareName(result.getConnPoolName());
+        String shaDowFileExtedn = convert.getShaDowFileExtedn();
+        if (StringUtils.isNotBlank(shaDowFileExtedn) &&
+                (DsTypeEnum.SHADOW_REDIS_SERVER.getCode().equals(convert.getDsType())
+                        || DsTypeEnum.SHADOW_DB.getCode().equals(convert.getDsType()))) {
+            // 获取影子数据源和地址
+            JSONObject dataObj = JSON.parseObject(shaDowFileExtedn);
+            String shadowUrl = dataObj.getString("shadowUrl");
+            String shadowPwd = dataObj.getString("shadowPwd");
+            String shadowUserName = dataObj.getString("shadowUserName");
+            dsEntity.setShadowDatabase(shadowUrl);
+            dsEntity.setShadowUserName(shadowUserName);
+            dsEntity.setShadowPassword(shadowPwd);
+        }
+        dsEntity.setAppName(convert.getApplicationName());
+        dsEntity.setBusinessUserName(convert.getUserName());
+        dsEntity.setBusinessDatabase(convert.getUrl());
+        dsEntity.setMiddlewareName(convert.getConnPoolName());
         dsEntity.setMiddlewareType(Type.MiddleWareType.LINK_POOL.value());
         return dsEntity;
     }

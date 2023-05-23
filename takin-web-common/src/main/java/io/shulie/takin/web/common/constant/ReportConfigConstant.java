bb22c1ebae1593692404d50db82b63d6ee389682
zz052831 <zz052831@163.com>
2023-05-23 17:28:45 +0800

    修复gcCost计算异常

--- takin-web-common/src/main/java/io/shulie/takin/web/common/constant/ReportConfigConstant.java
+++ takin-web-common/src/main/java/io/shulie/takin/web/common/constant/ReportConfigConstant.java
@@ -17,5 +17,5 @@ public class ReportConfigConstant {
     public static final String CHART_IO_KEY = "io";
     public static final String CHART_NETWORK_KEY = "network";
     public static final String CHART_GC_COUNT = "gcCount";
-    public static final String CHART_GC_COST = "gcCost";
+    public static final String CHART_GC_COST = "gcTime";
 }

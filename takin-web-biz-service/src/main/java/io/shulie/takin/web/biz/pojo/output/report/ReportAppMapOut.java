2fc6d8f512b10af2de192bd38d955ed5ade98570
zz052831 <zz052831@163.com>
2023-05-30 17:23:44 +0800

    修改bug

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/pojo/output/report/ReportAppMapOut.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/pojo/output/report/ReportAppMapOut.java
@@ -25,23 +25,11 @@ public class ReportAppMapOut {
     private Double successRate[];
 
     @ApiModelProperty(value = "请求数数组")
-    private Integer concurent[];
+    private Integer concurrent[];
 
     private String[] xtime;
 
     private String[] xcost;
 
-    private String[] conut;
-
-    public static void main(String[] args) {
-        ReportAppMapOut reportAppMapOut = new ReportAppMapOut();
-        reportAppMapOut.setAppName("appName");
-        Map<String, Integer> map = new LinkedHashMap<>();
-        map.put("1", 1);
-        reportAppMapOut.setTps(new Double[]{1.0, 2.0});
-        reportAppMapOut.setRt(new Double[]{1.0, 2.0});
-        reportAppMapOut.setSuccessRate(new Double[]{1.0, 2.0});
-        reportAppMapOut.setConcurent(new Integer[]{1, 2});
-        System.out.println(JSON.toJSONString(reportAppMapOut));
-    }
+    private String[] count;
 }

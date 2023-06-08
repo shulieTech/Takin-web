bddbe74cf6e202f4ee376847c8998a4dd4d38c7a
xiaojianzhang <xiaojianzhang@shulie.io>
2023-06-08 17:51:24 +0800

    fix：注释掉这个三个定时任务

--- takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/job/CalcApplicationSummaryJob.java
+++ takin-web-biz-service/src/main/java/io/shulie/takin/web/biz/job/CalcApplicationSummaryJob.java
@@ -22,13 +22,13 @@ import org.springframework.stereotype.Component;
  * @author 无涯
  * @date 2021/7/13 23:10
  */
-@Component
-@ElasticSchedulerJob(jobName = "calcApplicationSummaryJob",
-        // 分片序列号和参数用等号分隔 不需要参数可以不加
-        isSharding = true,
-        //shardingItemParameters = "0=0,1=1,2=2",
-        cron = "*/10 * * * * ?",
-        description = "汇总应用 机器数 风险机器数")
+//@Component
+//@ElasticSchedulerJob(jobName = "calcApplicationSummaryJob",
+//        // 分片序列号和参数用等号分隔 不需要参数可以不加
+//        isSharding = true,
+//        //shardingItemParameters = "0=0,1=1,2=2",
+//        cron = "*/10 * * * * ?",
+//        description = "汇总应用 机器数 风险机器数")
 @Slf4j
 public class CalcApplicationSummaryJob extends AbstractSceneTask implements SimpleJob {
 

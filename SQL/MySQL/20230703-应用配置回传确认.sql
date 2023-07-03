e4b5b1f223cc455d284c4f770210bea19f371c11
zhaoyong <zhaoyong@shulie.io>
2023-07-03 16:51:04 +0800

    sql添加

---
+++ SQL/MySQL/20230703-应用配置回传确认.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `trodb`.`t_application_mnt`
+    ADD COLUMN `conf_check_status` tinyint(2) NULL DEFAULT 1 COMMENT '配置校验状态，0下发中，1已生效' AFTER `cluster_name`,
+ADD COLUMN `conf_check_version` int NULL DEFAULT 0 COMMENT '配置版本，标识配置更新次数' AFTER `check_status`;
+

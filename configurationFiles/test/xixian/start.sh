65e5b92f08d439d4b3be39715a1cc218bcfcdaa2
李学森 <lixs151@chinaunicom.cn>
2023-08-04 17:04:55 +0800

    去除测试环境pinpoint启动参数

--- configurationFiles/test/xixian/start.sh
+++ configurationFiles/test/xixian/start.sh
@@ -4,11 +4,9 @@ dir=$PWD
 hostName=${HOSTNAME:0:16}
 
 JAVA_OPTS=\
-"-javaagent:$dir/pinpoint-agent-2.0.3-origin/pinpoint-bootstrap.jar \
--Dpinpoint.agentId=$hostName \
+"-Dpinpoint.agentId=$hostName \
 -Dpinpoint.applicationName=pressure_web_xixian_test \
 -Dpinpoint.licence=1d07fa023d02d60d \
--Dpinpoint.log=$dir/pinpoint-agent-2.0.3-origin/ \
 -Duser.timezone=Asia/Shanghai \
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
 -Xms3g \
